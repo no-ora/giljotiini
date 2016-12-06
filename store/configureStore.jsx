e06ef3b35202ae2969e9830047867dac99d0aad1
Noora Vainio <nooraevainio@gmail.com>
2016-12-06 22:26:01 +0200

    Fix bundle and add redux-logger

--- store/configureStore.jsx
+++ store/configureStore.jsx
@@ -1,10 +1,16 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from '../reducers';
 
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
+    applyMiddleware(thunk, promise, logger),
     window.devToolsExtension ? window.devToolsExtension() : undefined
   );
 
